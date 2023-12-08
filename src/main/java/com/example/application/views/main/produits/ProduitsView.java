a9f41055518936044d3f0dd225fec9f180d02f8f
Louis Carbo Estaque <louis@carbo-estaque.fr>
2023-12-08 11:33:26 +0100

    Ajout des différentes pages, à implémenter

---
+++ src/main/java/com/example/application/views/main/produits/ProduitsView.java
@@ -0,0 +1,56 @@
+package com.example.application.views.main.produits;
+
+import com.example.application.model.SamplePerson;
+import com.example.application.views.main.MainLayout;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@PageTitle("Produits")
+@Route(value = "produits", layout = MainLayout.class)
+@Uses(Icon.class)
+public class ProduitsView extends Div {
+
+    private Grid<SamplePerson> grid;
+
+    public ProduitsView() {
+        setSizeFull();
+        addClassNames("produits-view");
+
+        VerticalLayout layout = new VerticalLayout(createGrid());
+        layout.setSizeFull();
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        add(layout);
+    }
+
+    private Component createGrid() {
+        grid = new Grid<>(SamplePerson.class, false);
+        grid.addColumn("firstName").setAutoWidth(true);
+        grid.addColumn("lastName").setAutoWidth(true);
+        grid.addColumn("email").setAutoWidth(true);
+        grid.addColumn("phone").setAutoWidth(true);
+        grid.addColumn("dateOfBirth").setAutoWidth(true);
+        grid.addColumn("occupation").setAutoWidth(true);
+        grid.addColumn("role").setAutoWidth(true);
+
+        grid.setItems();
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.addClassNames(LumoUtility.Border.TOP, LumoUtility.BorderColor.CONTRAST_10);
+
+        return grid;
+    }
+
+    private void refreshGrid() {
+        grid.getDataProvider().refreshAll();
+    }
+
+}
